c6dcb0aed4b23043fe2dc53c88046b4f35105a8c
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-12-11 06:32:14 +0200

    fix server returning status 500 for non authorised users requests instead of 4xx

--- src/server/middlewares/auth_middleware.go
+++ src/server/middlewares/auth_middleware.go
@@ -9,12 +9,12 @@ import (
 // AuthMiddleware checks for valid x-authorization access token in the request headers
 func AuthMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		token := ctx.Request.Header["X-Authorization"][0]
-
-		if len(token) == 0 {
+		if len(ctx.Request.Header["X-Authorization"]) == 0 || len(ctx.Request.Header["X-Authorization"][0]) == 0 {
 			ctx.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
+
+		token := ctx.Request.Header["X-Authorization"][0]
 
 		claims, isValid, err := utils.ParseJWT(token)
 		if err != nil || !isValid {
